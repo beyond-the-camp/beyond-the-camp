5e966949077542f9a350054018d6105ca267fab7
Hentie Louw <hentie.louw@gmail.com>
2019-01-16 22:36:30 +0000

    Add postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,14 @@
 const postcssPresetEnv = require(`postcss-preset-env`);
+const postcssImport = require('postcss-import');
 const tailwindcss = require('tailwindcss');
 
 module.exports = () => ({
   plugins: [
     postcssPresetEnv({
       stage: 0
+    }),
+    postcssImport({
+      path: ['src/styles']
     }),
     tailwindcss('./tailwind.js'),
     require('autoprefixer')
