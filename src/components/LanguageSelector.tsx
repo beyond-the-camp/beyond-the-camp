284f1bf9d58ab271b5df1b4e43b29a58bfc57ceb
Hentie Louw <hentie.louw@gmail.com>
2019-03-10 10:36:22 +0000

    Use hooks to manage state in LanguageSelector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { useState } from 'react';
 import { Link } from 'gatsby';
 
 import { getLanguageName, getLocaleList } from '../utils/languages';
@@ -19,51 +20,27 @@ interface Props {
   links?: LocaleLinks;
 }
 
-interface State {
-  showMenu: boolean;
-}
+export const LanguageSelector = (props: Props) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const { currentLocale, links } = props;
 
-export class LanguageSelector extends React.Component<Props, State> {
-  public state: State = {
-    showMenu: false
-  };
-
-  public constructor(props: Props) {
-    super(props);
-    this.hideMenu = this.hideMenu.bind(this);
-    this.toggleMenu = this.toggleMenu.bind(this);
-  }
-
-  public render() {
-    const { currentLocale, links } = this.props;
-    return (
-      <div className="relative inline-block">
-        <button
-          onClick={this.toggleMenu}
-          className="hover:opacity-50 text-white"
-        >
-          {getLanguageName(currentLocale)}
-        </button>
-        {this.state.showMenu && (
-          <div className="absolute block pin-r bg-white p-3 z-50 border-black shadow-md">
-            {getLocaleList().map(locale => (
-              <p key={locale} className="m-0">
-                <Link to={getLink(locale, links)}>
-                  {getLanguageName(locale)}
-                </Link>
-              </p>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  private hideMenu() {
-    this.setState({ showMenu: false });
-  }
-
-  private toggleMenu() {
-    this.setState(state => ({ showMenu: !state.showMenu }));
-  }
-}
+  return (
+    <div className="relative inline-block">
+      <button
+        onClick={() => setShowMenu(prevShowMenu => !prevShowMenu)}
+        className="hover:opacity-50 text-white"
+      >
+        {getLanguageName(currentLocale)}
+      </button>
+      {showMenu && (
+        <div className="absolute block pin-r bg-white p-3 z-50 border-black shadow-md">
+          {getLocaleList().map(locale => (
+            <p key={locale} className="m-0">
+              <Link to={getLink(locale, links)}>{getLanguageName(locale)}</Link>
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
