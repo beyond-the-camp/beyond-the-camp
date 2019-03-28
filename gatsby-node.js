7a9b48ad44a14f65fc6e66332e19cd69e774bf9b
Hentie Louw <hentie.louw@gmail.com>
2019-03-28 20:14:33 +0000

    Remove some unused code

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
 /*eslint-env node*/
 
 const { createFilePath } = require(`gatsby-source-filesystem`);
-const path = require(`path`);
-const routes = require('./src/utils/routes');
-const { getLocaleList, getPrimaryLocale } = require('./src/utils/languages');
+// const path = require(`path`);
+// const routes = require('./src/utils/routes');
+// const { getLocaleList, getPrimaryLocale } = require('./src/utils/languages');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,43 +8,26 @@ import { Layout } from '../components/Layout';
 import { ListCard } from '../components/ListCard';
 import { getLanguageName, getLocaleList } from '../utils/languages';
 
-export default class LanguagePage extends React.Component {
-  // private static loadLocale(): string {
-  //   return localStorage.getItem('locale');
-  // }
+const LanguagePage = () => (
+  <Layout>
+    <div className="container mx-auto">
+      <ul className="list-reset flex flex-wrap -mx-2 mt-4">
+        {getLocaleList().map(locale => (
+          <li key={locale} className="w-full max-w-sm md:w-1/2 px-2 mb-2">
+            <Link
+              to={locale}
+              onClick={() => LanguagePage.storeLocale(locale)}
+              className="no-underline"
+            >
+              <ListCard>
+                <div>{getLanguageName(locale)}</div>
+              </ListCard>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </Layout>
+);
 
-  private static storeLocale(locale: string) {
-    localStorage.setItem('locale', locale);
-  }
-
-  // public componentDidMount() {
-  //   const locale = LanguagePage.loadLocale();
-  //   if (locale) {
-  //     navigate(locale);
-  //   }
-  // }
-
-  public render() {
-    return (
-      <Layout>
-        <div className="container mx-auto">
-          <ul className="list-reset flex flex-wrap -mx-2 mt-4">
-            {getLocaleList().map(locale => (
-              <li key={locale} className="w-full max-w-sm md:w-1/2 px-2 mb-2">
-                <Link
-                  to={locale}
-                  onClick={() => LanguagePage.storeLocale(locale)}
-                  className="no-underline"
-                >
-                  <ListCard>
-                    <div>{getLanguageName(locale)}</div>
-                  </ListCard>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </Layout>
-    );
-  }
-}
+export default LanguagePage;
